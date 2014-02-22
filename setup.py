9874705f81a0e649031adf62edcd0f460ae31125
Austin Hartzheim <austin@austinhartzheim.me>
2014-02-22 05:20:41 +0000

    v1.0.0 - Added short description to setup.py and set the version number.

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from distutils.core import setup
 
 setup(
     name='Trixy',
-    version='1.0.0b0',
+    version='1.0.0',
     packages=['trixy'],
     license='GNU GPL v3',
-    long_description=open('README.txt').read()
+    description='Python proxy server',
+    long_description=open('README.txt').read(),
+    url='austinhartzheim.me/projects/python3-trixy/'
 )
