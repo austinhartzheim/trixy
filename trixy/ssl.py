eead6129b0b1f6d86a00c454e5404cd29cb64649
Austin Hartzheim <austin@austinhartzheim.me>
2014-12-07 07:52:59 +0000

    Python 3.1 not working with Travis CI. Oh Well.

--- trixy/ssl.py
+++ trixy/ssl.py
@@ -4,17 +4,20 @@ import trixy
 
 
 class TrixySSLInput(trixy.TrixyInput):
-    pass  # TODO: implement
+    def __init__(self, sock, addr, **kwargs):
+        super().__init__(sock, addr)
+        self.socket = ssl.wrap_socket(self.socket, server_side=True, **kwargs)
 
 
 class TrixySSLOutput(trixy.TrixyOutput):
-    def __init__(self, host, port, autoconnect=True):
-        super().__init__(host, port, autoconnect=False)
+    def __init__(self, host, port, autoconnect=True, **kwargs):
+        super().__init__(host, port, autoconnect=False, **kwargs)
 
         if autoconnect:
             self.connect((host, port))
 
-    def setup_socket(self, host, port, autoconnect):
+    def setup_socket(self, host, port, autoconnect, **kwargs):
         addr_info = socket.getaddrinfo(host, port)
-        sock = ssl.wrap_socket(socket.socket(addr_info[0][0], addr_info[0][1]))
+        sock = ssl.wrap_socket(socket.socket(addr_info[0][0], addr_info[0][1])
+                               ** kwargs)
         self.set_socket(sock)
