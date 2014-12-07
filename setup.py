721d357e0e08b4b6e4bb0f4be38e1026e73c5396
Austin Hartzheim <austin@austinhartzheim.me>
2014-12-07 07:06:20 +0000

    Release Trixy 2.0.0b0pre
    Rewrote all code, providing a completely new interface to Trixy. There is now
    a clear separation between servers, inputs, processors, and outputs, allowing
    for more obvious code structure and readability.
    
    Additionally, the trixy.ssl code is now available. At present, only the
    trixy.ssl.TrixySSLOutput is working. This should make it easier to interact
    with remote sessions that use encryption while still allowing unencrypted
    access to the data in Trixy.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Trixy',
     author='Austin Hartzheim',
 
-    version='2.0.0',
+    version='2.0.0b0pre',
     packages=['trixy'],
     license='GNU GPL v3',
     description='Python proxy server',
