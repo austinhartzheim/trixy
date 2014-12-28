de90a9b43b4cb6449d1831a6527b408ca896e493
Austin Hartzheim <austin@austinhartzheim.me>
2014-12-28 19:54:50 +0000

    The Socks4aInput is now a subclass of Socks4Input

--- trixy/proxy.py
+++ trixy/proxy.py
@@ -106,26 +106,18 @@ class Socks4Input(trixy.TrixyInput):
                               socket.inet_aton(addr)))
 
 
-class Socks4aInput(trixy.TrixyInput):
+class Socks4aInput(Socks4Input):
     '''
     Implements the SOCKS4a protocol, which is the same as the SOCKS4
     protocol except for the addition of DNS resolution as described
     here: http://www.openssh.com/txt/socks4a.protocol
     '''
-        # TODO: decide if binding will be allowed. Probably off by default
+    # TODO: decide if binding will be allowed. Probably off by default
     #   but can be enabled by an option in __init__?
     def __init__(self, sock, addr):
         super().__init__(sock, addr)
         print('Got connect')
         self.first_packet = True
-
-    def handle_packet_down(self, data):
-        print('pd: ', data)
-        if self.first_packet:
-            self.handle_proxy_request(data)
-            self.first_packet = False
-            return
-        self.forward_packet_down(data)
 
     def handle_proxy_request(self, data):
         '''
@@ -165,48 +157,6 @@ class Socks4aInput(trixy.TrixyInput):
         # TODO: need functionality to detect if the connection fails to
         #   notify the application accordingly.
         self.reply_request_granted(addr, port)
-
-    def reply_request_granted(self, addr, port):
-        '''
-        Send a reply stating that the connection or bind request has
-        been granted and that the connection or bind attempt was
-        successfully completed.
-        '''
-        # 90 is the response for a granted request
-        self.send(struct.pack('!BBH4p', 0x00, 90, port,
-                              socket.inet_aton(addr)))
-
-    def reply_request_failed(self, addr, port):
-        '''
-        Send a reply stating that the request was rejected (perhaps due
-        to a firewall rule forbidding the connection or binding) or
-        that it failed (i.e., the remote host could not be connected to
-        or the requested port could not be bound).
-        '''
-        # 91 is the response for a rejected or failed request
-        self.send(struct.pack('!BBH4p', 0x00, 91, port,
-                              socket.inet_aton(addr)))
-
-    def reply_request_rejected(self, addr, port):
-        '''
-        Send a reply saying that the request was rejected because the
-        SOCKS server could not connect to the client's identd server.
-        '''
-        # 92 is the response for a request being rejected because the SOCKS
-        #   server cannot connect to identd on the client.
-        self.send(struct.pack('!!BBH4p', 0x00, 92, port,
-                              socket.inet_aton(addr)))
-
-    def reply_request_rejected_id_mismatch(self, addr, port):
-        '''
-        Send a reply saying that the request was rejected because the
-        SOCKS server was sent an ID by the client that did not match
-        the ID returned by identd on the client's computer.
-        '''
-        # 93 is the response for rejections due to the client program and
-        #   identd reporting different user-ids.
-        self.send(struct.pack('!!BBH4p', 0x00, 93, port,
-                              socket.inet_aton(addr)))
 
 
 class Socks5Input(trixy.TrixyInput):
